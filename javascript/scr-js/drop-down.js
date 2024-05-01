2bfb7f003ceb738b7207b55ddb051ebbb2405dcb
eurie <euriepaulino92@gmail.com>
2024-05-01 19:58:05 +0800

    SCROLL ANIMATION FIX COLORS

--- javascript/scr-js/drop-down.js
+++ javascript/scr-js/drop-down.js
@@ -7,3 +7,15 @@
                 const isOpen = dropDown.classList.contains('open')
                 toggleBtn1.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
             }
+
+
+            const  sr = ScrollReveal({
+                origin: 'top',
+                distance: '80px',
+                duration: 2500,
+                delay: 300, 
+
+            })
+
+            sr.reveal(`.card`, {delay: 500, interval: 100})
+            sr.reveal(`.head`)
